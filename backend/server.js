a61043d69bfdaa544b6a32f729f29bde5f5dd28d
yhanz15 <yhanz.kd15@gmail.com>
2024-10-13 03:53:11 +0800

    updating

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,33 @@
-import app from "./app.js";
-import 'dotenv/config';
-import mongoose from 'mongoose';
+require('dotenv').config();
+const mongoose = require('mongoose');
+const app = require('./app');
 
-const { DB_HOST } = process.env;
-const PORT = 3500;
+const { HOST_DB, PORT = 3000 } = process.env;
 
-mongoose
-	.connect(DB_HOST,{})
-	.then(() => {
-		app.listen(PORT, () => {
-		  console.log(`Server is running. Use our API on port: ${PORT}`);
-		});
-		console.log('Connected to database!');
-	})
-	.catch(e => {
-		console.log(`Error: ${e.message}`);
-		process.exit(1);
-	})
+// Set Mongoose strictQuery option
+mongoose.set('strictQuery', true); // or false based on your requirement
+
+const main = async () => {
+  try {
+    if (!HOST_DB) {
+      throw new Error("HOST_DB not set!");
+    }
+    
+    console.log("HOST_DB:", HOST_DB); // Log the HOST_DB variable to verify it's loaded
+
+    await mongoose.connect(HOST_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connection successful");
+    app.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`Server is listening on port: ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Error:", error.message);
+    process.exit(1);
+  }
+};
+
+main();
