07a63a1c62af17f35217bd339a0e9a25df43e551
GSevensT <143946090+GSevensT@users.noreply.github.com>
2024-10-14 17:17:25 +0800

    fix import header

--- frontend/src/components/SharedLayout/SharedLayout.jsx
+++ frontend/src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,6 @@
 import css from "./SharedLayout.module.css";
-import Header from "../Header/Header";
+//import Header from "../Header/Header";
+import Header from "components/header/Header";
 import { Outlet } from "react-router-dom";
 import { Background } from "../Background/Background";
 export const SharedLayout = () => {
