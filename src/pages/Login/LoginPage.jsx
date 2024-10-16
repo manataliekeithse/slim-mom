1e78b430a12fcbc62f87b8ad6493be377017e01d
manataliekeithse <141150584+manataliekeithse@users.noreply.github.com>
2024-10-16 19:43:37 +0800

    edited by nat

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,23 +1,42 @@
-import { signin } from "../../redux/auth/operations";
-import { useDispatch } from "react-redux";
+import { signin } from '../../redux/auth/operations';
+import { useDispatch } from 'react-redux';
+import css from './LoginPage.css';
 
 export const LoginPage = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	const handleOnSubmit = e => {
-		e.preventDefault();
-		const form = e.target;
-		
-		dispatch(signin({ 
-			email: form.elements.email.value,
-			password: form.elements.password.value
-		}));
-	}
-	return (
-		<form onSubmit={handleOnSubmit}>
-			<input type="text" name="email" autoComplete="false" />
-			<input type="password" name="password" autoComplete="false" />
-			<button>login</button>
-		</form>
-	);
-}
+  const handleOnSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+
+    dispatch(
+      signin({
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+  };
+  return (
+    <form className="login-container" onSubmit={handleOnSubmit}>
+      <h2 className="login-title">
+        <b>LOG IN</b>
+      </h2>
+      <input
+        type="text"
+        name="email"
+        placeholder="Email"
+        className="login-input"
+        autoComplete="false"
+      />
+      <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        className="login-input"
+        autoComplete="false"
+      />
+      <button className="btn">Login</button>
+      <button className="btn">Register</button>
+    </form>
+  );
+};
